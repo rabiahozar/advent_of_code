ef8c2f54287181bd2cfcdc4f408a6e1e57b3fc6d
rh <rabiahozar@gmail.com>
2022-04-25 20:46:13 +0200

    Day 5 helper clas

--- 2021/4/bingo.cpp
+++ 2021/4/bingo.cpp
@@ -3,38 +3,11 @@
 #include <set>
 #include <algorithm>
 
+#include <helper.h>
 #include <bingo.h>
 
 namespace advent
 {
-
-namespace helper
-{
-auto comma_sepeated_string_to_vector(const std::string& string, std::vector<int>& numbers) -> void
-{
-    std::stringstream streamer(string);
-    for (int number ; streamer >> number;)
-    {
-        numbers.push_back(number);
-        while (streamer.peek() == ',' || streamer.peek() == ' ')
-        {
-            streamer.ignore();
-        }
-    }
-}
-auto space_sepeated_string_to_vector(const std::string& string, std::vector<int>& numbers) -> void
-{
-    std::stringstream streamer(string);
-    for (int number ; streamer >> number;)
-    {
-        numbers.push_back(number);
-        while (streamer.peek() == ' ')
-        {
-            streamer.ignore();
-        }
-    }
-}
-} // namespace helper
 
 auto Day4::ReadInput() -> int
 {
